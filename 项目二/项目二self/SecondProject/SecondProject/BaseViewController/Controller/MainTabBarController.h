e2d9d5997cfafc9809cbbccf322cc409ab62b020
xiachenga <15757172115@163.com>
2016-07-29 16:33:27 +0800

    新创建了MainTabBar

---
+++ 项目二/项目二self/SecondProject/SecondProject/BaseViewController/Controller/MainTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.h
+//  SecondProject
+//
+//  Created by xiacheng on 16/7/29.
+//  Copyright © 2016年 wuxianhulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTabBarController : UITabBarController
+
+@end
